9656d1a830e8dca25c62b7b7eab851f1fb1f06a9
tatsuya-ohtani <dagutatsu@gmail.com>
2024-08-03 20:17:31 +0900

    0803

--- Quiz/ListViewController.swift
+++ Quiz/ListViewController.swift
@@ -13,7 +13,7 @@ class ListViewController: UIViewController {
     @IBOutlet var detailsLabel: UILabel!
     @IBOutlet var imageview: UIImageView!
     
-    class Animal {
+    class Xcode {
         var name: String
         var details: String
         var imageName: String
@@ -29,14 +29,17 @@ class ListViewController: UIViewController {
         }
     }
     
-    var animalArrey: [Animal] = [
-        Animal(name:"xcode", details: " iOSアプリケーションを開発するアプリケーションの名前", imageName: "kirin.jpg"),
-        Animal(name: "Swift", details: "iOSアプリケーションを開発する言語の名前", imageName: "penguin.jpg"),
-        Animal(name: "String",details: "文字列をあつかいたいときに使う型",imageName: "kangaroo.jpg"),
-        Animal(name: "Int",details: "整数をあつかいたいときに使う型",imageName: "kangaroo.jpg"),
-        Animal(name: "Bool",details: "条件判定や制御に使用される",imageName: "kangaroo.jpg"),
-        Animal(name: "＋ - * / %",details: "左から、足し算をする、引き算をする、掛け算をする、割り算をする、割った余りを求める",imageName: "kangaroo.jpg"),
-        Animal(name: "配列(リスト)の中身[]の呼び出し方",details: "数字や単語などの要素をを並べたとき、最初の要素はarrey[0]で最初の要素を呼び出し、arrey[１]で次の要素を呼び出す",imageName: "kangaroo.jpg"),
+    var listArrey: [Xcode] = [
+        Xcode(name: "xcode", details: " iOSアプリケーションを開発するアプリケーションの名前", imageName: "kirin.jpg"),
+        Xcode(name: "Swift", details: "iOSアプリケーションを開発する言語の名前", imageName: "penguin.jpg"),
+        Xcode(name: "変数の宣言① var 変数名:型(= 値)", details:"基本的的な変数を宣言する時の書き方。変数には型があり、その種類によって入れられる値が変わる", imageName: "penguin.jpg"),
+        Xcode(name: "String",details: "文字列をあつかいたいときに使う型",imageName: "kangaroo.jpg"),
+        Xcode(name: "Int",details: "整数をあつかいたいときに使う型",imageName: "kangaroo.jpg"),
+        Xcode(name: "Bool",details: "条件判定や制御に使用される",imageName: "kangaroo.jpg"),
+        Xcode(name: "＋ - * / %",details: "左から、足し算をする、引き算をする、掛け算をする、割り算をする、割った余りを求める",imageName: "kangaroo.jpg"),
+        Xcode(name: "変数の宣言②　@IBOutlet var 変数名:　型!", details: "Main.storyboard上のパーツをコードと関連付けるための差し込み口。そのパーツの名前が変数の型となり、それにより関連付けできるパーツが変わる。！を忘れずに。", imageName: "kangaroo.jpg"),
+        Xcode(name: "UILabel型、UIImageView型、UIButton型", details: "代表的な@IBOutlet変数の型。関連づけるパーツが左からLabel、ImageView、Buttonの時に使う。", imageName: "kangaroo.jpg"),
+        Xcode(name: "配列(リスト)の中身[]の呼び出し方",details: "数字や単語などの要素をを並べたとき、最初の要素はarrey[0]で最初の要素を呼び出し、arrey[１]で次の要素を呼び出す",imageName: "kangaroo.jpg"),
         
         
         
@@ -52,7 +55,7 @@ class ListViewController: UIViewController {
     }
     
     @IBAction func next() {
-        if index == animalArrey.count - 1 {
+        if index == listArrey.count - 1 {
             index = 0
         }else {
             index += 1
@@ -63,7 +66,7 @@ class ListViewController: UIViewController {
     
     @IBAction func back() {
         if index == 0 {
-            index = animalArrey.count - 1
+            index = listArrey.count - 1
         }else {
             index -= 1
         }
@@ -71,9 +74,9 @@ class ListViewController: UIViewController {
     }
     
     func setUI() {
-       // imageview.image = animalArrey[index].getImage()
-        nameLabel.text = animalArrey[index].name
-        detailsLabel.text = animalArrey[index].details
+       // imageview.image = listArrey[index].getImage()
+        nameLabel.text = listArrey[index].name
+        detailsLabel.text = listArrey[index].details
     }
 
 
